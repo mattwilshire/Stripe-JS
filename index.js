fa941c9962d7abcfcdf9ff9930c85575b5140e15
Matt <matthewwilshire1@gmail.com>
2022-12-29 02:29:01 +0000

    Subscriptions / Webhook for subscription paid

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.get('/', async (req, res) => {
 
 app.get('/charge', async (req, res) => {
 	let stripeToken = req.query.stripeToken;
+
+	// Shouldn't really create a customer every time
+	// Just create one once, then use the customer id for every transaction, store customer id in DB.
 	let customer = await stripe.customers.create({
 		email: 'matt@gmail.com',
 		name: 'Matt',
@@ -33,6 +36,47 @@ app.get('/charge', async (req, res) => {
 		customer: customer.id
 	});
 	res.send(charge);
+});
+
+// Subscribe customer id to the product that is a recurring daily payment!
+app.get('/sub', async (req, res) => {
+	const subscription = await stripe.subscriptions.create({
+		customer: 'cus_N4JhJlIsG4iZnQ',
+		items: [
+			{price: 'price_1MKBDtHhnv4PluXK6WeCQ0gj'},
+		],
+	});
+	res.send(subscription);
+});
+
+const endpointSecret = process.env.ESECRET;
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  switch (event.type) {
+    case 'invoice.paid':
+      const invoice = event.data.object;
+      // PAID FOR SUBSCRIPTION!
+      console.log("Invoice paid!!");
+      break;
+    case 'customer.created':
+      console.log("Customer created!");
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  response.send();
 });
 
 app.listen(PORT, () => {
